399383477eb5e37c81d2b06184e070b70cf96fc4
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-06-26 23:10:20 +0700

    style: update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"white-checker": "#eceed5",
+				"black-checker": "#78955d",
+			},
+		},
 	},
 	plugins: [],
 };
